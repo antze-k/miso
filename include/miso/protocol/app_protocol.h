bdf4f07839f27b5e50fd15c2abb3aa4560084d0d
antze-k <unresolved-external@singu-lair.com>
2018-02-24 17:30:51 +0300

    build fix

--- include/miso/protocol/app_protocol.h
+++ include/miso/protocol/app_protocol.h
@@ -5,6 +5,8 @@
 #include <cstdint>
 #include <vector>
 
+#include <stddef.h>
+
 namespace miso {
 //------------------------------------------------------------------------------
 
--- include/miso/protocol/app_protocol_simple.h
+++ include/miso/protocol/app_protocol_simple.h
@@ -4,8 +4,6 @@
 
 #include "miso/protocol/app_protocol.h"
 
-#include <stddef.h>
-
 namespace miso {
 //------------------------------------------------------------------------------
 
