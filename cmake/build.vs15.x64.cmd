a3f7eadf302a9c22c791ee7b898c9d550641712c
antze-k <unresolved-external@singu-lair.com>
2018-02-24 14:11:09 +0300

    build scripts

---
+++ cmake/build.vs15.x64.cmd
@@ -0,0 +1,15 @@
+@echo off
+set SCRIPT=%~0
+for %%F in ("%SCRIPT%") do set SCRIPT_DIR=%%~dpF
+
+set BUILD_DIR=%SCRIPT_DIR%vs15.x64
+set SRC_DIR=%SCRIPT_DIR%
+
+echo Creating build directory %BUILD_DIR%
+mkdir %BUILD_DIR%
+pushd %BUILD_DIR%
+
+cmake -G "Visual Studio 15 2017 Win64" %SRC_DIR%
+cmake --build . --config MinSizeRel
+
+popd
